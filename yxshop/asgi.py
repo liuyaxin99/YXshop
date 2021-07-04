f87e71de9621f7911fb5fcf203dcd86e452b8509
LiuYaxin <1294672229@qq.com>
2021-07-04 19:41:07 +0800

    this is my first commit

---
+++ yxshop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yxshop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yxshop.settings')
+
+application = get_asgi_application()
